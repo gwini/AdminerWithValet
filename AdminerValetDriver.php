231c710eae5ed359ffe3d267042942b292a07096
Jan Cimbál <honzacimbal@gmail.com>
2021-02-08 16:48:16 +0100

    Added adminer valet driver

---
+++ AdminerValetDriver.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Class AdminerValetDriver
+ */
+class AdminerValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string $sitePath
+     * @param  string $siteName
+     * @param  string $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath.'/adminer.php');
+    }
+
+    /**
+     * Determine if the incoming request is for a adminer.css file.
+     * @see https://www.adminer.org/#extras
+     *
+     * @param  string $sitePath
+     * @param  string $siteName
+     * @param  string $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if (file_exists($staticFilePath = $sitePath.'/adminer.css')) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string $sitePath
+     * @param  string $siteName
+     * @param  string $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        return $sitePath.'/adminer.php';
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # AdminerWithValet
 Laravel Valet driver for adminer
+
+
+## Summary 
+This driver allows you to run adminer using [laravel/valet](https://github.com/laravel/valet)
+
+## Usage
+- download [Adminer](https://www.adminer.org/en/)
+- If you want to install custom theme - follow these steps: [https://www.adminer.org/#extras](https://www.adminer.org/#extras)
+    - name of css file is important -> filename: '_adminer.css_'
+
